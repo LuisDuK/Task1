d61c62e9608744af825e86876e246ee41082c9ef
LuisDuk <minhduc8a178@gmail.com>
2025-10-24 22:04:51 +0700

    update 24-10

--- obj/Debug/net8.0/QLHangHoa.AssemblyInfo.cs
+++ obj/Debug/net8.0/QLHangHoa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLHangHoa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+080246bea4cac73fbc854a10303da478abd35a0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfe09845588976686214be8d06a0c829e49aefb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLHangHoa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLHangHoa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
